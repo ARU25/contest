615f88185419b9b0729728e43e160756c6f925d0
Abdul <abdulraheem.usmani@assaabloy.com>
2019-05-11 21:32:22 +0530

    admin app validations and flow changes

--- src/main/resources/static/admin-app.js
+++ src/main/resources/static/admin-app.js
@@ -22,15 +22,67 @@ angular.module('contestAdmin', [
                 'rank': result.rank
             }
         });
-    }
+    };
 
-    contestantService.query().$promise.then(function(response){
+
+    resultService.query().$promise.then(function(results){
+        _this.resultsDeclared = results.length > 0;
+        return contestantService.query().$promise;
+    }).then(function(response){
             _this.documents = response;
         });
 
 
     _this.download = function(fileId){
         documentService.get({"id":fileId});
+    }
+
+    _this.rankEntered = function(){
+        if(!_this.documents){
+            return false;
+        }
+        return _this.documents.filter(function(document){
+           return document.rank;
+        }).length > 0;
+    }
+
+    _this.validateResult = function(documents){
+        if(!documents)
+            return false;
+        var rankMap = {};
+        var isValid = true;
+        documents.forEach(function(document){
+            if(!document.rank){
+                return;
+            }
+            if(rankMap[document.rank]){
+                isValid = false;
+            }
+            rankMap[document.rank] = document;
+        });
+
+        if(!isValid){
+            return isValid;
+        }
+
+        var ranks = Object.keys(rankMap);
+        if (ranks.length == 0){
+            return false;
+        }
+        var sortedRanks = ranks.sort(function(a, b){
+           return a>b;
+        });
+
+        sortedRanks.forEach(function(element,index){
+            if(index-1 >=0){
+                isValid = isValid && (parseInt(element) == (parseInt(sortedRanks[index-1]) + 1));
+            } else if (parseInt(element) !== 1){
+                isValid = false;
+            }
+        });
+
+        return isValid;
+
     }
 
     _this.saveResult = function(documents){
